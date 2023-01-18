d983560976671b18969791f7fc1bee05c7471bbc
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-01-18 08:50:36 -0600

    Fix Postgres initialization
    The `docker-entrypoint-initdb.d` directory was misreferenced in the
    docker-compose file.
    Consequently, when the container was started, the volume was created
    automatically (as indicated [by the
    documentation](https://docs.docker.com/storage/volumes/#start-a-container-with-a-volume)):
    > If you start a container with a volume that doesn’t yet exist, Docker creates the volume for you.
    
    Once the reference was changed to the `.devcontainer` directory in the
    host, where an `init.sql` exists, the initialization script was run as
    expected during the _initial_ creation of the container.
    
    Note that in order to get this script to run, there cannot be an
    existing database.
    Fundamentally, this means that the volumes cannot exist.

--- .devcontainer/docker-entrypoint-initdb.d/init.sql
+++ .devcontainer/docker-entrypoint-initdb.d/init.sql
@@ -1,3 +1,22 @@
-CREATE USER docker;
-	CREATE DATABASE docker_crud;
-	GRANT ALL PRIVILEGES ON DATABASE docker_crud TO docker;
+CREATE USER docker_user;
+  -- CREATE DATABASE docker_crud; -- this is unnecessary because it's defined as an environment variable?
+  GRANT ALL PRIVILEGES ON DATABASE docker_crud TO docker_user;
+
+-- Add uuid-ossp for primary keys
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS accounts (
+	user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+
+INSERT INTO accounts (username, password, email, created_on)
+VALUES
+('user1', 'value1', 'user1@gmail.com', now()),
+('user2', 'value2', 'user2@gmail.com', now()),
+('user3', 'value3', 'user3@gmail.com', now()),
+('user4', 'value4', 'user4@gmail.com', now());
