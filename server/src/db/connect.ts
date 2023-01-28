da460237edbed65d0d47b118ad5a364a9247c2f1
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-01-28 15:18:14 -0600

    Swap client for pool
    Instead of returning a client, we now export a pool and use the connect
    to establish the connection to the pool.
    This makes it much easier to reuse the pool later.

--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -1,23 +1,9 @@
-import { Client, ClientConfig } from 'pg';
-import { loadEnv } from '../utils/loadEnv';
+import { pool } from "./pool";
 
-loadEnv();
-
-const config: ClientConfig = {
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
-}
-
-export const createConnection = async (): Promise<Client> => {
-  console.log({config})
+export const createConnection = async (): Promise<void> => {
   try {
-    const client = new Client(config);
-    await client.connect();
+    await pool.connect();
     console.log('🚀 Db connected successfully')
-    return client
   } catch (e) {
     console.error(e)
     throw new Error('Error while connecting to the PG client',);
